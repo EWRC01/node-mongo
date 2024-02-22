74efdd9ec721732eef3aae9b0b6eb3c317b40240
EWRC01 <edwin1026@hotmail.es>
2024-02-22 11:54:03 -0600

    Add MVC pattern to project and add .env.example file to show to user how to connect to database

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('./db');
-const routes = require('./routes');
+const db = require('./models/db');
+const userController = require('./controllers/userController');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/', routes);
+app.use('/', userController);
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
